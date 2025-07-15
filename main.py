1ea84ce24cd7a25b210243f2db6f1379edde30d9
LaBalbaChuky <73657940+LaBalbaChuky@users.noreply.github.com>
2025-07-15 16:12:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask import Flask, render_template
-from scraper import obtener_resultados, agrupar_loterias, guardar_historial
+from scraper import obtener_resultados, agrupar_loterias, crear_html, guardar_html
+from upload_to_netlify import subir_a_netlify
 import os  # ⬅️ NECESARIO
 
 app = Flask(__name__)
@@ -31,5 +32,11 @@ def actualizar():
     return f"✅ Datos actualizados: {timestamp}", 200
 
 if __name__ == "__main__":
+    loterias, timestamp = obtener_resultados()
+    grupos = agrupar_loterias(loterias)
+    html = crear_html(grupos, timestamp)
+    guardar_html(html)  # Guarda en public/index.html
+    subir_a_netlify()   # Sube a Netlify automáticamente
+    
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
