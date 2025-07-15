a65ddd8045a09d5eccd4f06474919634c6e80236
LaBalbaChuky <73657940+LaBalbaChuky@users.noreply.github.com>
2025-07-15 17:23:25 -0400

    Update upload_to_netlify.py

--- upload_to_netlify.py
+++ upload_to_netlify.py
@@ -2,9 +2,9 @@ import requests
 import requests
 
 def subir_a_netlify():
-    url = "https://api.netlify.com/api/v1/sites/<SITE_ID>/deploys"
+    url = "https://api.netlify.com/api/v1/sites/<837f7e6f-1855-4f9a-ad30-4a0bc29bdd72>/deploys"
     headers = {
-        "Authorization": "Bearer <TOKEN>"
+        "Authorization": "Bearer <nfp_UErELT16pDTr8x4z9aLR96GtcpHiEZ6M16dc>"
     }
 
     files = {
@@ -13,4 +13,3 @@ def subir_a_netlify():
 
     response = requests.post(url, headers=headers, files=files)
     print(f"✅ Subida a Netlify: {response.status_code}", response.text)
-
