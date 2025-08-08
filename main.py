ee1652995f5e0316545ea57a0351d227906eca4a
LaBalbaChuky <73657940+LaBalbaChuky@users.noreply.github.com>
2025-08-08 14:42:56 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from scraper import obtener_resultados, agrupar_loterias, crear_html, guardar_html
-
-if __name__ == "__main__":
-    loterias, actualizacion = obtener_resultados()
-    grupos = agrupar_loterias(loterias)
-    html = crear_html(grupos, actualizacion)
-    guardar_html(html)
